576b8cdcb153bc8e6f4b828ab1bb60876865fa62
MrPiccolo23 <sattar1994@hotmail.com>
2022-02-09 08:43:05 +0000

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,33 @@
+import img1 from './img/British-Shorthair.png';
+import img2 from './img/Maine-coon.png';
+import img3 from './img/bengal-cat.png';
+
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'British Shorthair',
+            desc:'',
+            price: 100,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Maine Coon',
+            type:'',
+            price: 200,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Bengal cat',
+            desc:' ',
+            price: 300,
+        },
+    ]
+
+};
+
+export default data;
